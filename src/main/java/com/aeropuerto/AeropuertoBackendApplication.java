e3a89361019c25e6675c8204f319a83a73799530
javiluli <javiluli.dev@gmail.com>
2022-05-22 10:16:02 +0200

    feat(initial-release): first commit

---
+++ src/main/java/com/aeropuerto/AeropuertoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aeropuerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeropuertoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeropuertoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeropuerto/AeropuertoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeropuerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeropuertoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
